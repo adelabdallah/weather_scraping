7227efa10b18b858f1081081a3d5c2328f2266ff
Adel <adeloabdallah1995@gmail.com>
2018-08-15 00:13:57 -0400

    Cleaning results. Need to eliminate duplicates.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-weather_scraping
+Simple project to scrape Toronto weather from Environment Canada.
+
+Primary purpose of this project is to learn basic web access with python.
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup
 page = requests.get('https://weather.gc.ca/city/pages/on-143_metric_e.html')
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# daytimeForecastList = soup.select('wxRow.span.wxperiod_temp.daytime')
+# secondSet = soup.find_all('section', class_='visible-xs mrgn-tp-md')
+# secondSet.decompose()
+
 daytimeForecastList = soup.find_all('span', class_='high wxo-metric-hide')
 
+
 for forecast in daytimeForecastList:
-        print(forecast.prettify())
+        print(forecast.contents[0])
