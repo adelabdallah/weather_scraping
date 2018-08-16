0a982a3022f4017288788f7cf22ae860b0fddf2c
Adel <adeloabdallah1995@gmail.com>
2018-08-15 21:30:07 -0400

    Eliminated duplicate values in a silly way

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from bs4 import BeautifulSoup
 page = requests.get('https://weather.gc.ca/city/pages/on-143_metric_e.html')
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# secondSet = soup.find_all('section', class_='visible-xs mrgn-tp-md')
-# secondSet.decompose()
-
 daytimeForecastList = soup.find_all('span', class_='high wxo-metric-hide')
 
-
-for forecast in daytimeForecastList:
-        print(forecast.contents[0])
+for i in range(6):
+    print(daytimeForecastList[i].contents[0])
